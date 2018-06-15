6596e339ebdae51c7cd02d0ce5a84c3c6b25d2c8
m-takata <m_takata_1974@yahoo.co.jp>
2018-06-15 18:29:11 +0900

    ・-paramオプションで、変数マップを指定できるようにした。 ・ScriptEngineInterpreterServiceのコマンド実行機能を追加した。

--- src/main/java/jp/ossc/nimbus/service/interpreter/ScriptEngineInterpreterService.java
+++ src/main/java/jp/ossc/nimbus/service/interpreter/ScriptEngineInterpreterService.java
@@ -31,10 +31,14 @@
  */
 package jp.ossc.nimbus.service.interpreter;
 
+import java.io.*;
 import java.util.*;
+import java.beans.*;
 import javax.script.*;
 
 import jp.ossc.nimbus.core.*;
+import jp.ossc.nimbus.beans.ServiceNameEditor;
+import jp.ossc.nimbus.beans.NimbusPropertyEditorManager;
 
 /**
  * ScriptEngineを使ってJavaコードを実行するインタープリタサービス。<p>
@@ -233,5 +237,240 @@ public class ScriptEngineInterpreterService extends ServiceBase
         }catch(ScriptException e){
             throw new EvaluateException(e);
         }
+    }
+    
+    private static void usage(){
+        System.out.println("コマンド使用方法：");
+        System.out.println(" java jp.ossc.nimbus.service.interpreter.ScriptEngineInterpreterService [options] [source code]");
+        System.out.println();
+        System.out.println("[options]");
+        System.out.println();
+        System.out.println(" [-servicepath paths]");
+        System.out.println("  インタープリタサービスの起動に必要なサービス定義ファイルのパスを指定します。");
+        System.out.println("  パスセパレータ区切りで複数指定可能です。");
+        System.out.println();
+        System.out.println(" [-servicename name]");
+        System.out.println("  インタープリタサービスのサービス名を指定します。");
+        System.out.println("  指定しない場合はNimbus#Interpreterとみなします。");
+        System.out.println();
+        System.out.println(" [-file paths]");
+        System.out.println("  実行するソースコードファイルを指定します。");
+        System.out.println("  パスセパレータ区切りで複数指定可能です。");
+        System.out.println("  このオプションの指定がない場合は、引数source codeでソースコードを指定します。");
+        System.out.println();
+        System.out.println(" [-param name(type)=value]");
+        System.out.println("  スクリプト変数として渡す変数名と型と値を指定します。");
+        System.out.println("  型宣言である(type)は省略可能で、省略した場合は、java.lang.Stringとなります。");
+        System.out.println();
+        System.out.println(" [-encoding encode]");
+        System.out.println("  実行するソースコードファイルの文字コードを指定します。");
+        System.out.println();
+        System.out.println(" [-help]");
+        System.out.println("  ヘルプを表示します。");
+        System.out.println();
+        System.out.println("[source code]");
+        System.out.println(" 実行するソースコードを指定します。");
+        System.out.println();
+        System.out.println(" 使用例 : ");
+        System.out.println("    java -classpath classes;lib/nimbus.jar jp.ossc.nimbus.service.interpreter.ScriptEngineInterpreterService Java.type(\"java.lang.System\").out.println(\"test\");");
+    }
+    
+    private static List parsePaths(String paths){
+        String pathSeparator = System.getProperty("path.separator");
+        final List result = new ArrayList();
+        if(paths == null || paths.length() == 0){
+            return result;
+        }
+        if(paths.indexOf(pathSeparator) == -1){
+            result.add(paths);
+            return result;
+        }
+        String tmpPaths = paths;
+        int index = -1;
+        while((index = tmpPaths.indexOf(pathSeparator)) != -1){
+            result.add(tmpPaths.substring(0, index));
+            if(index != tmpPaths.length() - 1){
+                tmpPaths = tmpPaths.substring(index + 1);
+            }else{
+                tmpPaths = null;
+                break;
+            }
+        }
+        if(tmpPaths != null && tmpPaths.length() != 0){
+            result.add(tmpPaths);
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) throws Exception{
+        
+        if(args.length == 0 || (args.length != 0 && args[0].equals("-help"))){
+            usage();
+            if(args.length == 0){
+                System.exit(-1);
+            }
+            return;
+        }
+        
+        boolean option = false;
+        String key = null;
+        ServiceName serviceName = null;
+        List servicePaths = null;
+        List files = null;
+        String encode = null;
+        Map params = null;
+        StringWriter code = new StringWriter();
+        PrintWriter codeWriter = new PrintWriter(code);
+        for(int i = 0; i < args.length; i++){
+            if(option){
+                if(key.equals("-servicename")){
+                    ServiceNameEditor editor = new ServiceNameEditor();
+                    editor.setAsText(args[i]);
+                    serviceName = (ServiceName)editor.getValue();
+                }else if(key.equals("-servicepath")){
+                    servicePaths = parsePaths(args[i]);
+                }else if(key.equals("-file")){
+                    files = parsePaths(args[i]);
+                }else if(key.equals("-encoding")){
+                    encode = args[i];
+                }else if(key.equals("-param")){
+                    int index = args[i].indexOf("=");
+                    if(index == -1){
+                        System.out.println("パラメータが不正です : " + args[i]);
+                        System.exit(-1);
+                    }
+                    String name = args[i].substring(0, index);
+                    String value = args[i].substring(index + 1);
+                    Class type = String.class;
+                    index = name.indexOf("(");
+                    if(index != -1 && name.charAt(name.length() - 1) == ')'){
+                        try{
+                            type = Utility.convertStringToClass(name.substring(index + 1, name.length() - 1));
+                        }catch(ClassNotFoundException e){
+                            System.out.println("パラメータの型クラスが見つかりません : " + args[i]);
+                            System.exit(-1);
+                        }
+                        name = name.substring(0, index);
+                    }
+                    PropertyEditor editor = NimbusPropertyEditorManager.findEditor(type);
+                    if(editor == null){
+                        System.out.println("パラメータの型が編集できない型です : " + args[i]);
+                        System.exit(-1);
+                    }
+                    editor.setAsText(value);
+                    if(params == null){
+                        params = new HashMap();
+                    }
+                    params.put(name, editor.getValue());
+                }
+                option = false;
+                key = null;
+            }else{
+                if(args[i].equals("-servicename")
+                     || args[i].equals("-servicepath")
+                     || args[i].equals("-file")
+                     || args[i].equals("-encoding")
+                     || args[i].equals("-param")
+                ){
+                    option = true;
+                    key = args[i];
+                }else if(args[i].equals("-help")){
+                    usage();
+                    return;
+                }else{
+                    codeWriter.print(" " + args[i]);
+                }
+            }
+        }
+        Interpreter interpreter = null;
+        if(servicePaths == null){
+            ScriptEngineInterpreterService service = new ScriptEngineInterpreterService();
+            service.create();
+            service.start();
+            interpreter = service;
+        }else{
+            for(int i = 0, imax = servicePaths.size(); i < imax; i++){
+                if(!ServiceManagerFactory.loadManager((String)servicePaths.get(i))){
+                    System.out.println("Service load error." + servicePaths.get(i));
+                    Thread.sleep(1000);
+                    System.exit(-1);
+                }
+            }
+            if(!ServiceManagerFactory.checkLoadManagerCompleted()){
+                Thread.sleep(1000);
+                System.exit(-1);
+            }
+            if(serviceName == null){
+                serviceName = new ServiceName("Nimbus", "Interpreter");
+            }
+            interpreter = (Interpreter)ServiceManagerFactory
+                .getServiceObject(serviceName);
+        }
+        if(files != null){
+            code.flush();
+            for(int i = 0, imax = files.size(); i < imax; i++){
+                FileInputStream fis = new FileInputStream((String)files.get(i));
+                InputStreamReader isr = null;
+                if(encode == null){
+                    isr = new InputStreamReader(fis);
+                }else{
+                    isr = new InputStreamReader(fis, encode);
+                }
+                BufferedReader br = new BufferedReader(isr);
+                String line = null;
+                while((line = br.readLine()) != null){
+                    codeWriter.println(line);
+                }
+                fis.close();
+            }
+        }
+        codeWriter.flush();
+        int exitCode = 0;
+        try{
+            System.out.println(params == null ? interpreter.evaluate(code.toString()) : interpreter.evaluate(code.toString(), params));
+        }catch(Throwable e){
+            StringBuilder buf = new StringBuilder();
+            final String lineSeparator = System.getProperty("line.separator");
+            buf.append("Exception occuers :").append(e.toString()).append(lineSeparator);
+            final StackTraceElement[] elemss = e.getStackTrace();
+            if(elemss != null){
+                for(int i = 0; i < elemss.length; i++){
+                    buf.append('\t');
+                    if(elemss[i] != null){
+                        buf.append(elemss[i].toString()).append(lineSeparator);
+                    }else{
+                        buf.append("null").append(lineSeparator);
+                    }
+                }
+            }
+            for(Throwable ee = getCause(e); ee != null; ee = getCause(ee)){
+                buf.append("Caused by:").append(ee.toString()).append(lineSeparator);
+                final StackTraceElement[] elems = ee.getStackTrace();
+                if(elems != null){
+                    for(int i = 0; i < elems.length; i++){
+                        buf.append('\t');
+                        if(elems[i] != null){
+                            buf.append(elems[i].toString()).append(lineSeparator);
+                        }else{
+                            buf.append("null").append(lineSeparator);
+                        }
+                    }
+                }
+            }
+            System.out.println(buf.toString());
+            exitCode = -1;
+        }finally{
+            if(servicePaths != null){
+                for(int i = servicePaths.size(); --i >= 0;){
+                    ServiceManagerFactory.unloadManager((String)servicePaths.get(i));
+                }
+            }
+        }
+        System.exit(exitCode);
+    }
+    
+    private static Throwable getCause(Throwable th){
+        Throwable cause = th.getCause();
+        return cause == th ? null : cause;
     }
 }
