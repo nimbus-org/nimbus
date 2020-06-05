a5a79e9995dade51e187c1643daf355e4af88ed6
m-takata <34699171+m-takata@users.noreply.github.com>
2020-06-05 20:28:12 +0900

    Issue #611対応 (#612)

--- src/main/java/jp/ossc/nimbus/service/interpreter/ScriptEngineInterpreterServiceMBean.java
+++ src/main/java/jp/ossc/nimbus/service/interpreter/ScriptEngineInterpreterServiceMBean.java
@@ -42,28 +42,166 @@ import jp.ossc.nimbus.core.*;
  */
 public interface ScriptEngineInterpreterServiceMBean extends ServiceBaseMBean{
     
+    /**
+     * スクリプトエンジンを特定するための、拡張子を設定する。<p>
+     *
+     * @param ext 拡張子
+     */
     public void setExtension(String ext);
+    
+    /**
+     * スクリプトエンジンを特定するための、拡張子を取得する。<p>
+     *
+     * @return 拡張子
+     */
     public String getExtension();
     
+    /**
+     * スクリプトエンジンを特定するための、MIMEタイプを設定する。<p>
+     *
+     * @param type MIMEタイプ
+     */
     public void setMimeType(String type);
+    
+    /**
+     * スクリプトエンジンを特定するための、MIMEタイプを取得する。<p>
+     *
+     * @return MIMEタイプ
+     */
     public String getMimeType();
     
+    /**
+     * スクリプトエンジンを特定するための、エンジン名を設定する。<p>
+     *
+     * @param name エンジン名
+     */
     public void setEngineName(String name);
+    
+    /**
+     * スクリプトエンジンを特定するための、エンジン名を取得する。<p>
+     *
+     * @return エンジン名
+     */
     public String getEngineName();
     
+    /**
+     * グローバルスコープ変数を設定する。<p>
+     *
+     * @param name 変数名
+     * @param val 値
+     */
     public void setGlobalBinding(String name, Object val);
+    
+    /**
+     * グローバルスコープ変数を取得する。<p>
+     *
+     * @param name 変数名
+     * @return 値
+     */
     public Object getGlobalBinding(String name);
+    
+    /**
+     * グローバルスコープ変数マップを取得する。<p>
+     *
+     * @return 変数マップ
+     */
     public Map<String, Object> getGlobalBindings();
     
+    /**
+     * エンジンスコープ変数を設定する。<p>
+     *
+     * @param name 変数名
+     * @param val 値
+     */
     public void setEngineBinding(String name, Object val);
+    
+    /**
+     * エンジンスコープ変数を取得する。<p>
+     *
+     * @param name 変数名
+     * @return 値
+     */
     public Object getEngineBinding(String name);
+    
+    /**
+     * エンジンスコープ変数マップを取得する。<p>
+     *
+     * @return 変数マップ
+     */
     public Map<String, Object> getEngineBindings();
     
-    public void setClassLoader(ClassLoader loader);
-    public ClassLoader getClassLoader();
-    
+    /**
+     * コンパイル毎にスクリプトエンジンを生成するかどうかを設定する。<p>
+     * デフォルトは、false。
+     *
+     * @param isNew コンパイル毎にスクリプトエンジンを生成する場合、true
+     */
     public void setNewScriptEngineByEvaluate(boolean isNew);
+    
+    /**
+     * コンパイル毎にスクリプトエンジンを生成するかどうかを判定する。<p>
+     *
+     * @return trueの場合、コンパイル毎にスクリプトエンジンを生成する
+     */
     public boolean isNewScriptEngineByEvaluate();
     
+    /**
+     * スクリプトをファンクションでラップするかどうかを設定する。<p>
+     * デフォルトは、false。
+     *
+     * @param isWrap スクリプトをファンクションでラップする場合、true
+     * @see #setWrapperFunction(String)
+     * @see #setStepDelimitor(String)
+     */
+    public void setWrapByFunction(boolean isWrap);
+    
+    /**
+     * スクリプトをファンクションでラップするかどうかを判定する。<p>
+     *
+     * @return trueの場合、スクリプトをファンクションでラップする
+     */
+    public boolean isWrapByFunction();
+    
+    /**
+     * スクリプトをラップする関数テンプレートを設定する。<p>
+     * デフォルトは、
+     * <pre>
+     * function wrapper(){$script\nreturn $lastStep}\nwrapper();
+     * </pre>
+     * 関数テンプレートに、$lastStepが含まれる場合は、スクリプトから{@link #setStepDelimitor(String)}で指定されたステップのデリミターを使って、最終ステップとそれ以外に分割し、テンプレート中の$scriptと$lastStepに埋め込む。ステップのデリミターを使った、最終ステップの特定は、厳密な言語仕様の解釈をして、見つける訳ではないため、スクリプトの書き方に注意が必要です。<br>
+     * $lastStepが含まれない場合は、テンプレート中の$scriptに埋め込む。<br>
+     *
+     * @param wrapper スクリプトをラップする関数テンプレート
+     */
+    public void setWrapperFunction(String wrapper);
+    
+    /**
+     * スクリプトをラップする関数テンプレートを取得する。<p>
+     *
+     * @return スクリプトをラップする関数テンプレート
+     */
+    public String getWrapperFunction();
+    
+    /**
+     * スクリプトをラップする関数テンプレートの最終ステップを探索する際に使用するステップのデリミターを設定する。<p>
+     * デフォルトは、";"。<br>
+     *
+     * @param delimitor デリミター
+     */
+    public void setStepDelimitor(String delimitor);
+    
+    /**
+     * スクリプトをラップする関数テンプレートの最終ステップを探索する際に使用するステップのデリミターを取得する。<p>
+     *
+     * @return デリミター
+     */
+    public String getStepDelimitor();
+    
+    /**
+     * 指定されたスクリプトをインタプリタ実行する。<br>
+     *
+     * @return 実行結果
+     * @exception EvaluateException インタプリタ実行中に例外が発生した場合
+     */
     public Object evaluate(String code) throws EvaluateException;
 }
