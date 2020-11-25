fc396a9417bea85243ce26ec6736126b5edb8aee
ishidam <34698707+ishidamu@users.noreply.github.com>
2020-11-25 12:37:38 +0900

    Revert "#695 (#698)"
    This reverts commit bf321baf7815ac2e2216406f3fcecfb1f99eb4ad.

--- src/main/java/jp/ossc/nimbus/service/keepalive/ClusterService.java
+++ src/main/java/jp/ossc/nimbus/service/keepalive/ClusterService.java
@@ -335,38 +335,12 @@ public class ClusterService extends ServiceBase implements Cluster, ClusterServi
         }
     }
     
-    public String getMemberString(){
-        return getMembers() == null ? "" : getMembers().toString();
-    }
-    
-    public int getMemberSize(){
-        if(members == null){
-            return 0;
-        }
-        synchronized(members){
-            return members.size();
-        }
-    }
-    
     public Set getClientMembers(){
         if(clientMembers == null){
             return null;
         }
         synchronized(clientMembers){
             return new HashSet(clientMembers.values());
-        }
-    }
-    
-    public String getClientMemberString(){
-        return getClientMembers() == null ? "" : getClientMembers().toString();
-    }
-    
-    public int getClientMemberSize(){
-        if(clientMembers == null){
-            return 0;
-        }
-        synchronized(clientMembers){
-            return clientMembers.values().size();
         }
     }
     
